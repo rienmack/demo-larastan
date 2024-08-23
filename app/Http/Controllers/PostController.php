890dbf2e1a3d5c81cd0e421c0c85aac08d244537
Ryan MacKenzie <rienmack@gmail.com>
2024-08-23 07:37:06 -0400

    test

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //    public function index($id, $foo)
+    {
+        Post::class;
+        dd(foo)
+        break;
+        return;
+        if ($id == 1) {
+            return 'Post 1';
+        } else {
+            return 'Post 2';
+        }
+    }
+}
